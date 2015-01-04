8d86d6ee22b225156c64766e293cbe6ee28d251c
Tim Falzone <timfalzone@Macintosh-2.local>
2015-01-04 13:55:11 -0500

    checkpoint

--- tsdb/tsdb.go
+++ tsdb/tsdb.go
@@ -7,63 +7,134 @@ import (
 	"github.com/couchbaselabs/go-slab"
 	"math"
 	"sort"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/cardamaro/telemetry"
 )
 
-type Row struct {
-	Var     string
-	Samples []*Sample
+type Row interface {
+	Var() string
+	Samples() []Sample
 }
 
-type Sample struct {
-	Timestamp time.Time
-	Value     float64
+type row struct {
+	varname          string
+	samples          []Sample
+	max, min, median Sample
 }
 
-func (s *Sample) String() string {
-	return fmt.Sprintf("<%.3f @ %s>", s.Value, s.Timestamp)
+func (r *row) Var() string       { return r.varname }
+func (r *row) Samples() []Sample { return r.samples }
+func (r *row) String() string {
+	return fmt.Sprintf("%s: %v", r.varname, r.samples)
+}
+
+type Sample interface {
+	Timestamp() time.Time
+	Value() float64
+}
+
+type baseSample struct {
+	timestamp time.Time
+	value     float64
+}
+
+func (s *baseSample) Timestamp() time.Time { return s.timestamp }
+func (s *baseSample) Value() float64       { return s.value }
+func (s *baseSample) String() string {
+	return fmt.Sprintf("<%.3f @ %s>", s.value, s.timestamp)
 }
 
 type TimeseriesDatabase interface {
 	Record(metric string, tags map[string]string, value float64)
 	Query(metric string, tags map[string]string) []*Row
+	Sum(metric string, filterTags map[string]string, groupBy []string) []*Row
 }
 
 type tsdb struct {
 	s          map[string]uint32
 	ss         map[uint32]string
-	metrics    map[uint32][][]byte
+	metrics    map[string][][]byte
 	tagsets    map[uint32][]byte
 	stagsets   map[string]uint32
 	arena      *slab.Arena
+	mu         *sync.RWMutex
 	tagCounter *telemetry.AtomicUint32
 }
 
+const MAX_STRING_ID uint32 = 1<<32 - 1
+
 func NewTsdb() *tsdb {
-	arena := slab.NewArena(24, 1024*1024, 2, nil)
+	arena := slab.NewArena(64, 1024*1024, 2, nil)
 
 	t := &tsdb{
 		s:          make(map[string]uint32),
 		ss:         make(map[uint32]string),
-		metrics:    make(map[uint32][][]byte),
+		metrics:    make(map[string][][]byte),
 		tagsets:    make(map[uint32][]byte),
 		stagsets:   make(map[string]uint32),
 		arena:      arena,
 		tagCounter: new(telemetry.AtomicUint32),
+		mu:         new(sync.RWMutex),
 	}
 	t.tagCounter.Add(10000000)
+
 	return t
 }
 
-func (t *tsdb) getIdForString(tag string) uint32 {
-	if v, ok := t.s[tag]; ok {
+var intern = make(map[string]string)
+
+func (t *tsdb) internString(s string) string {
+	if v, ok := intern[s]; ok {
+		return v
+	}
+	intern[s] = s
+	return s
+}
+
+func (t *tsdb) getOrAddIdForString(s string) uint32 {
+	s = t.internString(s)
+	if v, ok := t.s[s]; ok {
 		return v
 	}
 	v := t.tagCounter.Add(1)
-	t.s[tag] = v
-	t.ss[v] = tag
+	if v == MAX_STRING_ID {
+		panic("MAX_STRING_ID reached")
+	}
+	t.s[s] = v
+	t.ss[v] = s
+	return v
+}
+
+func (t *tsdb) getIdForString(s string) uint32 {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	if v, ok := t.s[s]; ok {
+		return v
+	}
+	return MAX_STRING_ID
+}
+
+func (t *tsdb) getIdForTags(tags map[string]string) uint32 {
+	keys := sortAndFilterTags(tags)
+	tbuf := t.arena.Alloc(8 * len(keys))
+	for i, k := range keys {
+		binary.BigEndian.PutUint32(tbuf[i*8:], t.getOrAddIdForString(k))
+		binary.BigEndian.PutUint32(tbuf[4+(i*8):], t.getOrAddIdForString(tags[k]))
+	}
+	strtbuf := t.internString(string(tbuf))
+	if v, ok := t.stagsets[strtbuf]; ok {
+		t.arena.DecRef(tbuf)
+		return v
+	}
+	v := t.tagCounter.Add(1)
+	if v == MAX_STRING_ID {
+		panic("MAX_STRING_ID reached")
+	}
+	t.tagsets[v] = tbuf
+	t.stagsets[strtbuf] = v
 	return v
 }
 
@@ -86,53 +157,52 @@ func (t *tsdb) Stats() {
 	sc := m["numSlabClasses"]
 	for i := int64(0); i < sc; i++ {
 		prefix := fmt.Sprintf("slabClass-%06d-", i)
-		fmt.Println(prefix)
 		used += m[prefix+"chunkSize"] * m[prefix+"numChunksInUse"]
 	}
-	fmt.Printf("Total size: %d\n", used/(1024*1024))
+	fmt.Printf("Total size: %d\n", used)
 	fmt.Printf("Stats: %+v\n", m)
 }
 
 func (t *tsdb) Record(metric string, tags map[string]string, timestamp time.Time, value float64) {
-	keys := sortAndFilterTags(tags)
-	b := t.arena.Alloc(16 + (8 * len(keys)))
-
-	binary.BigEndian.PutUint64(b, uint64(timestamp.UnixNano()))
+	b := t.arena.Alloc(4 + 4 + 8)
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	binary.BigEndian.PutUint32(b, uint32(timestamp.Unix()))
+	binary.BigEndian.PutUint32(b[4:], t.getIdForTags(tags))
 	binary.BigEndian.PutUint64(b[8:], math.Float64bits(value))
-
-	for i, k := range keys {
-		binary.BigEndian.PutUint32(b[16+(i*8):], t.getIdForString(k))
-		binary.BigEndian.PutUint32(b[20+(i*8):], t.getIdForString(tags[k]))
-	}
-
-	metricId := t.getIdForString(metric)
-	t.metrics[metricId] = append(t.metrics[metricId], b)
+	t.metrics[metric] = append(t.metrics[metric], b)
 	//fmt.Printf("recorded metric %s [%d] @ %s:\n% x\n", metric, t.getIdForString(metric), timestamp, b)
 }
 
-func (t *tsdb) Query(metric string, tags map[string]string) []*Row {
-	entry, ok := t.metrics[t.getIdForString(metric)]
+type rowProcessor func(tagBytes []byte, value float64, timestamp time.Time)
+
+func (t *tsdb) op(metric string, filterTags map[string]string, proc rowProcessor) {
+	t.mu.RLock()
+	entry, ok := t.metrics[metric]
+	t.mu.RUnlock()
 	if !ok {
-		return nil
+		return
 	}
 
-	keys := sortAndFilterTags(tags)
+	keys := sortAndFilterTags(filterTags)
 
+	//filter := make([]byte, 8 * len(keys))
 	filter := t.arena.Alloc(8 * len(keys))
+	defer t.arena.DecRef(filter)
+
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
 	for _, k := range keys {
 		binary.BigEndian.PutUint32(filter, t.getIdForString(k))
-		binary.BigEndian.PutUint32(filter[4:], t.getIdForString(tags[k]))
+		binary.BigEndian.PutUint32(filter[4:], t.getIdForString(filterTags[k]))
 	}
 
-	rows := make([]*Row, 0, 8)
-	out := make(map[string]*Row)
-
 	for _, b := range entry {
-		var value float64
-		include := true
+		timestamp := time.Unix(int64(binary.BigEndian.Uint32(b[0:4])), 0)
+		tagBytes := t.tagsets[binary.BigEndian.Uint32(b[4:8])]
 
-		timestamp := time.Unix(0, int64(binary.BigEndian.Uint64(b[0:8])))
-		tagBytes := b[16:]
+		include := true
 
 		for off := 0; off < 8*len(keys); off += 8 {
 			filterBytes := filter[off : off+8]
@@ -145,32 +215,103 @@ func (t *tsdb) Query(metric string, tags map[string]string) []*Row {
 		}
 
 		if include {
-			value = math.Float64frombits((binary.BigEndian.Uint64(b[8:16])))
-			row, ok := out[string(tagBytes)]
-			if !ok {
-				v := metric
-				for off := 0; off < len(tagBytes); off += 8 {
-					if off == 0 {
-						v += "{"
-					}
-					tag := t.ss[binary.BigEndian.Uint32(tagBytes[off:off+4])]
-					val := t.ss[binary.BigEndian.Uint32(tagBytes[off+4:off+8])]
-					v += tag + "=" + val
-					if off+8 == len(tagBytes) {
-						v += "}"
-					} else {
-						v += ","
-					}
-				}
-				row = &Row{Var: v}
-				out[string(tagBytes)] = row
-				rows = append(rows, row)
-			}
-			row.Samples = append(row.Samples, &Sample{timestamp, value})
+			value := math.Float64frombits((binary.BigEndian.Uint64(b[8:16])))
+			proc(tagBytes, value, timestamp)
 		}
 	}
+}
 
-	t.arena.DecRef(filter)
+func (t *tsdb) Sum(metric string, filterTags map[string]string, groupBy []string) []Row {
+	var rows []Row
+	var varname string
+	out := make(map[string]Sample)
+
+	var tags []string
+	for k, v := range filterTags {
+		tags = append(tags, k+"="+v)
+	}
+
+	hasGroupBy := len(groupBy) > 0
+	groupByIds := make(map[uint32]struct{})
+	if hasGroupBy {
+		t.mu.RLock()
+		for _, v := range groupBy {
+			groupByIds[t.getIdForString(v)] = struct{}{}
+		}
+		t.mu.RUnlock()
+	} else {
+		varname = metric + "{" + strings.Join(tags, ",") + "}"
+	}
+
+	t.op(metric, filterTags, func(tagBytes []byte, value float64, timestamp time.Time) {
+		if hasGroupBy {
+			gvtags := tags
+			matched := 0
+			for off := 0; off < len(tagBytes); off += 8 {
+				tt := binary.BigEndian.Uint32(tagBytes[off : off+4])
+				if _, ok := groupByIds[tt]; ok {
+					// only add this tag if it doesn't exist in the filterTags map
+					if filterTags[t.ss[tt]] == "" {
+						tv := binary.BigEndian.Uint32(tagBytes[off+4 : off+8])
+						gvtags = append(gvtags, t.ss[tt]+"="+t.ss[tv])
+					}
+					matched += 1
+				}
+			}
+			if matched != len(groupByIds) {
+				return
+			}
+			sort.Strings(gvtags)
+			gv := metric + "{" + strings.Join(gvtags, ",") + "}"
+			if out[gv] == nil {
+				out[gv] = new(baseSample)
+			}
+			out[gv].(*baseSample).value += value
+			out[gv].(*baseSample).timestamp = timestamp
+		} else {
+			if out[varname] == nil {
+				out[varname] = new(baseSample)
+			}
+			out[varname].(*baseSample).value += value
+			out[varname].(*baseSample).timestamp = timestamp
+		}
+	})
+
+	for k, v := range out {
+		row := &row{varname: k, samples: []Sample{v}}
+		rows = append(rows, row)
+	}
+
+	return rows
+}
+
+func (t *tsdb) Query(metric string, filterTags map[string]string) []Row {
+	var rows []Row
+	out := make(map[string]*row)
+
+	t.op(metric, filterTags, func(tagBytes []byte, value float64, timestamp time.Time) {
+		r, ok := out[string(tagBytes)]
+		if !ok {
+			v := metric
+			for off := 0; off < len(tagBytes); off += 8 {
+				if off == 0 {
+					v += "{"
+				}
+				tag := t.ss[binary.BigEndian.Uint32(tagBytes[off:off+4])]
+				val := t.ss[binary.BigEndian.Uint32(tagBytes[off+4:off+8])]
+				v += tag + "=" + val
+				if off+8 == len(tagBytes) {
+					v += "}"
+				} else {
+					v += ","
+				}
+			}
+			r = &row{varname: v}
+			out[string(tagBytes)] = r
+			rows = append(rows, r)
+		}
+		r.samples = append(r.samples, &baseSample{timestamp, value})
+	})
 
 	return rows
 }
--- tsdb/tsdb_test.go
+++ tsdb/tsdb_test.go
@@ -2,6 +2,7 @@ package tsdb
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 	"time"
 )
@@ -10,25 +11,41 @@ func TestQuery(t *testing.T) {
 	db := NewTsdb()
 	db.Record("foo.bar", map[string]string{"a": "b"}, time.Now(), 1.0)
 	db.Record("foo.bar", map[string]string{"a": "b"}, time.Now(), 2.0)
-	db.Record("foo.bar", map[string]string{"a": "b"}, time.Now(), 3.0)
-	db.Record("foo.bar", map[string]string{"a": "b", "c": "d"}, time.Now(), 4.0)
+	db.Record("foo.bar", map[string]string{"a": "c", "c": "d", "X": "Z"}, time.Now(), 3.0)
+	db.Record("foo.bar", map[string]string{"a": "c", "c": "e"}, time.Now(), 4.0)
 	r := db.Query("foo.bar", map[string]string{"a": "b"})
 	for _, row := range r {
-		fmt.Println(row.Var)
-		for _, s := range row.Samples {
+		fmt.Println(row.Var())
+		for _, s := range row.Samples() {
 			fmt.Println(s)
 		}
 	}
-	db.Stats()
+
+	fmt.Println("== Sum w/ Filter {a=b}")
+	r = db.Sum("foo.bar", map[string]string{"a": "b"}, nil)
+	fmt.Println(r)
+
+	fmt.Println("== Sum w/ Group By [a, c]")
+	r = db.Sum("foo.bar", nil, []string{"a", "c"})
+	fmt.Println(r)
+	r = db.Sum("foo.bar", map[string]string{"c": "d"}, []string{"a", "c"})
+	fmt.Println(r)
 }
 
 func TestQueryLarge(t *testing.T) {
 	db := NewTsdb()
 	start := time.Now()
+	var m1, m2 runtime.MemStats
+	runtime.ReadMemStats(&m1)
 	for i := 0; i < 100000; i++ {
-		nt := fmt.Sprintf("%d", i%10)
-		db.Record("foo.bar", map[string]string{nt: nt + nt}, time.Now(), float64(i))
+		nt := fmt.Sprintf("%d", i%100)
+		db.Record("foo.bar", map[string]string{"foo": nt}, time.Now(), float64(i))
 	}
+	runtime.GC()
+	runtime.ReadMemStats(&m2)
+	t.Logf("HeapAlloc: %d", m2.HeapAlloc)
+	t.Logf("HeapInuse: %d", m2.HeapInuse)
+	t.Logf("HeapObjects: %d", m2.HeapObjects)
 	t.Logf("duration: %s", time.Since(start))
 	db.Stats()
 
